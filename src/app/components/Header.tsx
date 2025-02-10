72eda74fefdfbca55414c30db9e9fddd384ddb5a
spaceishere <batocirdaramjav@gmail.com>
2025-02-10 20:07:30 +0800

    made header and home page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
+import { SiInstagram } from "react-icons/si";
+import { MdFacebook } from "react-icons/md";
+
 export default function Header() {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
+    <div className="flex h-[70px] w-full items-center justify-between bg-[#001B64] px-[5%]">
+      <Image
+        src="/Logo.png"
+        alt="logo"
+        width={130}
+        height={20}
+        className="h-[20px] w-[130px]"
+      />
+      <div className="flex gap-[20px] text-white">
+        <p>Бидний тухай</p>
+        <p>Үйлчилгээ</p>
+        <p>Барилгын материал</p>
+        <p>Авто худалдаа</p>
+        <p>Aвто сэлбэг</p>
+        <p>Талбай түрээс</p>
+      </div>
+      <div className="flex gap-[12px]">
+        <div className="flex h-[32px] w-[32px] items-center justify-center rounded-full bg-white">
+          <MdFacebook className="text-[20px]" />
+        </div>
+        <div className="flex h-[32px] w-[32px] items-center justify-center rounded-full bg-white">
+          <SiInstagram />
+        </div>
+      </div>
+    </div>
   );
 }
