88c52d3ac4be57e521c2a70bf74138547f7a07b4
delgerekhmendorshikh <delgerekhmendorshikh@gmail.com>
2025-07-01 16:25:21 +0800

    updated

--- src/layout.js
+++ src/layout.js
@@ -1,14 +1,14 @@
-"use client"
+"use client";
 
-import { ApolloProvider } from "@apollo/client"
-import "./globals.css"
-import client from "@/lib/apolloClient"
+import { ApolloProvider } from "@apollo/client";
+import "./globals.css";
+import client from "@/lib/apolloClient";
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body classname={`antialiased`}>
-        <ApolloPrvider client={client}>{children}</ApolloPrvider>
+        <ApolloProvider client={client}>{children}</ApolloProvider>
       </body>
     </html>
-  )
+  );
 }
